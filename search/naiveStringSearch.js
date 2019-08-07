5fe111051728bffd0f4ce9a07f563b6fee898cbe
peterk9 <kevpeters1@gmail.com>
2019-08-07 17:20:06 -0400

    naiveStringSearch

--- search/naiveStringSearch.js
+++ search/naiveStringSearch.js
@@ -1,8 +1,18 @@
-function naiveStringSearch(long, short) {
-    let currentIndex = 0;
-    for (let i = 0; i < long.length() - 1; i++) {
-        if (i <= short.length() - 1 && long[i] == short[i]) currentIndex++;
+function naiveStringSearch(arr, target) {
+    if (arr.length == 0 || target.length == 0) return 0;
+    if (target.length > arr.length) return -1;
+    let count = 0;
+    let match;
+    for (let i = 0; i < arr.length; i++) {
+        match = true;
+        for (let j = 0; j < target.length; j++)
+            if (arr[j + i] != target[j]) {
+                match = false;
+                break;
+            }
+        if (match) count++;
     }
+    return count;
 }
 
-naiveStringSearch("hello world hehe", "he");
+console.log(naiveStringSearch("hello world hehe", "he"));
