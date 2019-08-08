b1a9b2d76af927405c43008a33b3376f7c2999b6
peterk9 <kevpeters1@gmail.com>
2019-08-08 08:44:33 -0400

    improved naivestringsearch

--- search/linearSearch.js
+++ search/linearSearch.js
@@ -1,6 +1,8 @@
 function linearSearch(arr, v) {
     if (arr.length === 0) return -1;
-    for (let i = 0; i < arr.length; i++)
+    for (let i in arr)
         if (arr[i] === v) return i;
     return -1;
 }
+
+console.log(linearSearch([5, 3, 2, 77], 77))
--- search/naiveStringSearch.js
+++ search/naiveStringSearch.js
@@ -1,16 +1,12 @@
-function naiveStringSearch(arr, target) {
-    if (arr.length == 0 || target.length == 0) return 0;
-    if (target.length > arr.length) return -1;
+function naiveStringSearch(str, target) {
+    if (str.length == 0 || target.length == 0) return 0;
+    if (target.length > str.length) return -1;
     let count = 0;
-    let match;
-    for (let i = 0; i < arr.length; i++) {
-        match = true;
-        for (let j = 0; j < target.length; j++)
-            if (arr[j + i] != target[j]) {
-                match = false;
-                break;
-            }
-        if (match) count++;
+    for (let i = 0; i <= str.length - target.length; i++) {
+        let j;
+        for (j = 0; j < target.length; j++)
+            if (str[j + i] != target[j]) break;
+        if (j == target.length) count++;
     }
     return count;
 }
